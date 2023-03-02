b23aac34b7aa301c71dd5c68d0c5fb18ce0e58d1
Douglas Kadlec <colorghost@mac.lan>
2023-03-01 22:32:04 -0800

    deleted play and download buttons

--- js/tracks.js
+++ js/tracks.js
@@ -1,44 +1,38 @@
 export const tracks = [
   {
     title: 'Magic of the Mind',
-    artist: 'color ghost',
     length: '37:39',
     image: 'images/fiji.jpg',
-    path: 'https://colorghost.app/magic-of-the-mind.mp3'
+    url: 'https://colorghost.app/magic-of-the-mind.mp3'
   },
   {
     title: 'The Human Soul',
-    artist: 'color ghost',
     length: '46:25',
     image: 'images/night-sky.jpg',
-    path: 'https://colorghost.app/the-human-soul.mp3'
+    url: 'https://colorghost.app/the-human-soul.mp3'
   },
   {
     title: 'One In A Million We Are TWICE',
-    artist: 'color ghost',
     length: '1:07:49',
     image: 'images/twice.jpg',
-    path: 'https://colorghost.app/one-in-a-million-we-are-twice.mp3'
+    url: 'https://colorghost.app/one-in-a-million-we-are-twice.mp3'
   },
   {
     title: 'Another TWICE Mix to Feed Your Soul',
-    artist: 'color ghost',
     length: '53:06',
     image: 'images/predebut.jpg',
-    path: 'https://colorghost.app/another-twice-mix-to-feed-your-soul.mp3'
+    url: 'https://colorghost.app/another-twice-mix-to-feed-your-soul.mp3'
   },
   {
     title: 'Journey of the Mind',
-    artist: 'color ghost',
     length: '32:21',
     image: 'images/jisoo.jpg',
-    path: 'https://colorghost.app/journey-of-the-mind.mp3'
+    url: 'https://colorghost.app/journey-of-the-mind.mp3'
   },
   {
     title: 'Seize the Light',
-    artist: 'color ghost',
     length: '1:11:16',
     image: 'images/heart.jpg',
-    path: 'https://colorghost.app/seize-the-light.mp3'
+    url: 'https://colorghost.app/seize-the-light.mp3'
   }
 ]
